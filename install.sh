8e4b6e41d41e53343b7bbfb4017ecd1073ecde89
Makerbkz13 <adelparama@gmail.com>
2023-06-24 18:45:38 +0700

    VVIP TOOLS

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 apt update -y && apt upgrade -y
 pkg update -y && pkg upgrade-y
 pkg install wget
+pkg install python
 pkg install python3
 pkg install git
 pkg install figlet
+pip install requests
+pip install termcolor
+pip install colr
+pip install httpx
